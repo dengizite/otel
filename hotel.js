3c60b8d5ebe8d8cd993002b7dcb9f49795aa3aee
dengizite <egorkakos84@gmail.com>
2021-10-23 14:02:43 +0300

    5com

--- hotel.js
+++ hotel.js
@@ -1,8 +1,21 @@
 import express from 'express';import {default as http_base} from 'http'
 import {default as io_base} from 'socket.io';import path from 'path'
+import {default as mongodb} from 'mongodb';
+const MongoClient=mongodb.MongoClient,
+client=new MongoClient('mongodb+srv://dengizite:Egorka124@cluster0.p49dp.mongodb.net/hotel?retryWrites=true&w=majority'),
+event_close = "serverOpening", event_open = "serverClosed";
+
+client.on(event_close,ev=>{console.log(40,`received ${event_close}: ${JSON.stringify(ev, null, 2)}`)})
+client.on(event_open,ev=>{console.log(45,`received ${event_open}: ${JSON.stringify(ev, null, 2)}`)}) 
+
+let dbase
+async function con_mongo(){await client.connect();console.log('Connected');dbase=client.db('hotel').collection('dates')};con_mongo()
+
+function catch_err(err){console.log(195,err.message);if(err.message.includes('must be connected')===true){con_mongo()}}
+async function insrt_user(user){dbase.insertOne(user)}
 
 /* import {default as mongodb} from 'mongodb';
-
+mongodb+srv://dengizite:Egorka124@cluster0.p49dp.mongodb.net/hotel?retryWrites=true&w=majority
 const MongoClient=mongodb.MongoClient,
 client=new MongoClient('mongodb+srv://dbuser:81601312@cluster0.vcupc.mongodb.net/pref?retryWrites=true&w=majority'),
 event_close = "serverOpening", event_open = "serverClosed";
@@ -43,7 +56,9 @@ io.on('connection', (socket) => {
 
 	socket.on('get_data',(data)=>{console.log(data)
 		if(data[0]==='Бронирования'){console.log(data)}
-		else if(data[0]==='rooms_data'){io.to(socket.id).emit('get_data',['rooms_data',rooms])
+		else if(data[0]==='rooms_data'){
+			
+			io.to(socket.id).emit('get_data',['rooms_data',rooms])
 			//for(let i in rooms){}
 		}
 		else if(data[0]==='kl_data'){io.to(socket.id).emit('get_data',['kl_data',clients])}
@@ -52,7 +67,24 @@ io.on('connection', (socket) => {
 
 	socket.on('send_data',(data)=>{console.log(data)
 		if(data[0]==='add_client'){clients[data[2]]={name:data[1],pass:data[3],ident:data[4],tel:data[5]};console.log(52,clients)}
-		else if(data[0]==='add_room'){rooms[data[1]]={price:data[2],bad:data[3],cat:data[4],descr:data[5],stat:'свободен'};console.log(53,rooms)}
+		else if(data[0]==='add_room'){
+			rooms[data[1]]={price:data[2],bad:data[3],cat:data[4],descr:data[5],stat:'свободен'};console.log(53,rooms)
+
+
+			/* con_mongo().then(()=>{
+			db.users.insertOne({"name": "Tom", "age": 28, languages: ["english", "spanish"]})}) */
+
+
+
+
+			/* con_mongo().then(()=>{
+			insrt_user({[data[1]]:{'price':data[2],'bad':data[3],'cat':data[4],'descr':data[5],'stat':'свободен'}})})
+			.then((resp)=>{console.log(resp)})
+				.catch(err=>{catch_err(err)}) */
+				insrt_user({[data[1]]:{'price':data[2],'bad':data[3],'cat':data[4],'descr':data[5],'stat':'свободен'}})
+			.then((resp)=>{console.log(resp)})
+				.catch(err=>{catch_err(err)})
+		}
 	})
 
 	socket.on('chat',(msg)=>{console.log(11,msg)
